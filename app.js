d35b2790bce81069575236ad66a22f3ed28c10a4
Fabiano Eger <fabinhoeger@hotmail.com>
2017-06-21 20:19:53 +0000

    Added api server

---
+++ app.js
@@ -0,0 +1,24 @@
+'use strict';
+const express     = require('express');
+const body_parser = require('body-parser');
+const axios       = require('axios');
+const co          = require('co');
+const send_data   = require('./send_data');
+
+setInterval(send_data, 5000);
+
+const app = express();
+app.use(body_parser.json());
+
+app.post('/data', (req, res) => {
+const res_data = req.body;
+  app.get('/', (req, res) => {
+    res.send(`Tago check <br> Last data reiceved: ${JSON.stringify(res_data)}`);
+  });
+  console.log(req.body);
+}); 
+
+
+app.listen(8080);
+console.log("Servidor on!");
+
--- send_data.js
+++ send_data.js
@@ -1,10 +1,18 @@
 const axios = require('axios');
-const co = require('co');
-axios.defaults.baseURL = 'https://api.tago.io';
+const co    = require('co');
+
+// GLOBAL VARIABLES SETTINGS FROM AXIOS
+axios.defaults.baseURL                         = 'https://api.tago.io';
 axios.defaults.headers.common['Authorization'] = '3f276097-c197-4e74-be5a-dbcf3948fefa';
-axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.headers.post['Content-Type']    = 'application/json';
+
+module.exports = function sent_data() {
 co(function* () {
-    const req = yield axios.get('/data', { method: 'get' });
+    const data = {
+        variable: 'tago_check',
+        value: new Date().getTime(),
+    };
+    const req = yield axios.post('/data', data);
     console.log(req.data);
 
 }).catch((error) => {
@@ -18,4 +26,5 @@ co(function* () {
       console.log('Error', error.message);
     }
     console.log(error.config);
-  });
+  }).catch(console.log);
+}
